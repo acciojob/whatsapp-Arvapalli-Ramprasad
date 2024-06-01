c46b5bf72457686241cb01b1db3ba08d8695d296
Ramprasad <ramprasad6352@gmail.com>
2024-06-01 20:18:40 +0530

    User added

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -9,7 +9,12 @@ public class WhatsappRepository {
 
     //Assume that each user belongs to at most one group
     //You can use the below mentioned hashmaps or delete these and create your own.
+
+//    step1. Save user
+
+    private HashMap<String,String> user = new HashMap<>();
     private HashMap<Group, List<User>> groupUserMap;
+
     private HashMap<Group, List<Message>> groupMessageMap;
     private HashMap<Message, User> senderMap;
     private HashMap<Group, User> adminMap;
@@ -18,6 +23,7 @@ public class WhatsappRepository {
     private int messageId;
 
     public WhatsappRepository(){
+        this.user = new HashMap<String,String>();
         this.groupMessageMap = new HashMap<Group, List<Message>>();
         this.groupUserMap = new HashMap<Group, List<User>>();
         this.senderMap = new HashMap<Message, User>();
@@ -26,4 +32,14 @@ public class WhatsappRepository {
         this.customGroupCount = 0;
         this.messageId = 0;
     }
+    public String createUser(String name, String mobile) throws Exception {
+        if(userMobile.contains(mobile)){
+             throw new Exception("User already exists");
+        }else{
+            user.put(mobile,name);
+            userMobile.add(mobile);
+        }
+        return "SUCCESS";
+    }
+
 }
