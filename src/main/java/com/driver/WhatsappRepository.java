ce396c281a02d4a5d544586d1a0516bd0ed151e8
Ramprasad <ramprasad6352@gmail.com>
2024-06-02 12:37:16 +0530

    Send Message

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -73,7 +73,10 @@ public class WhatsappRepository {
 
         if(!groupUserMap.containsKey(group)){
             throw new Exception("Group does not exist");
-        } else if (groupUserMap.containsKey(group) && !groupUserMap.containsValue(sender)) {
+        }
+
+
+        if(!groupUserMap.get(group).contains(sender)) {
             throw new Exception("You are not allowed to send message");
         }
 
